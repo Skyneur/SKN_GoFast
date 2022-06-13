49f3bdd583a6ee1444083e6b2a9c8c18cda1cfa0
Skyneur <77664746+Skyneur@users.noreply.github.com>
2022-06-13 03:11:27 +0200

    Add files via upload

---
+++ SKN_GoFast/config.lua
@@ -0,0 +1,31 @@
+Config = {
+
+    MinReward = 800,
+
+    MaxReward = 2300,
+
+    VehicleFin = "bmx",
+
+    GoFastPos = {
+        {x = 1711.50, y = -1555.75, z = 113.94},
+    },
+
+    GoFastPosVente = {
+        [1] = {x = 439.49, y = 6459.89, z = 27.80},
+        [2] = {x = 257.44, y = 79.63, z = 98.95},
+        [3] = {x = -1174.51, y = -2061.56, z = 13.45},
+        [4] = {x = -1131.66, y = 2697.78, z = 17.80},
+    },
+
+    Vehicle = {
+        [1] = {label = "Stafford", model = "stafford"},
+        [2] = {label = "Tailgater", model = "tailgater"},
+        [3] = {label = "Burrito", model = "burrito3"},
+        [4] = {label = "Chino", model = "chino"},
+        [5] = {label = "Italigtb", model = "italigtb"}
+    },
+
+    SpawnPoint = {
+        {x = 1718.94, y = -1561.58, z = 112.15, h = 290.01}
+    },
+}
